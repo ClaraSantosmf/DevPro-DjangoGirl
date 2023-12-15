330f257b79ee32aba11fd6f83670d79e4bb310a7
Santos <clarasantosmf@gmail.com>
2023-12-14 21:44:45 -0300

    testes de django 5 no python anywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +26,9 @@ SECRET_KEY = 'django-insecure-wg2urcykep#506!nkn%xj5$naw&cl_x8e49l)2d)0@&)*nj8u7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
-
+APP_NAME = os.environ.get("FLY_APP_NAME")
+ALLOWED_HOSTS = ['127.0.0.1', f"{APP_NAME}.fly.dev"]  # ← Updated!
+CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
 # Application definition
 
 INSTALLED_APPS = [
